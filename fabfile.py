ec197ec0e57fe13abdedf6e72b369cb8c46cfb98
Roy Prins <prinsroy@gmail.com>
2016-07-31 21:07:01 +0200

    Testing for empty

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,10 @@ FREEZE_FOLDER = 'gh-pages'
 FREEZE_BRANCH = 'gh-pages'
 
 
-def commit_source():
+def commit_source(message):
     local("git add -A")
-    message = raw_input("Enter a source commit message:  ")
+    if not message:
+        message = raw_input("Enter a source commit message:  ")
     # Test for uncommited changes (avoid error or empty commit)
     local("git diff-index --quiet HEAD || git commit -m \"%s\" && git push origin master" % message)
 
@@ -17,14 +18,16 @@ def freeze():
         local("python freeze.py")
 
 
-def commit_static():
+def commit_website(message):
     with lcd(FREEZE_FOLDER):
         local("git add -A")
-        message = raw_input("Enter a webpage commit message:  ")
+        if not message:
+            message = raw_input("Enter a webpage commit message:  ")
         # Test for uncommited changes (avoid error or empty commit)
         local("git diff-index --quiet HEAD || git commit -m \"%s\" && git push origin %s" % (message, FREEZE_BRANCH))
 
 def deploy():
-    commit_source()
+    message = raw_input("Enter a commit message:  ")
+    commit_source(message)
     freeze()
-    commit_static()
+    commit_website(message)
