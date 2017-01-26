e5791fcce8a964a1c5560cdcb9e984963410e0af
Roy Prins <prinsroy@gmail.com>
2017-01-26 10:07:23 +0100

    New site layout

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 from flask import Flask, render_template
 
-#initialization
+# initialization
 app = Flask(__name__)
 
-#configuration
+# configuration
 app.config['FREEZER_DESTINATION'] = 'gh-pages'
 app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME', '.gitignore', 'readme.md']
 app.config['FREEZER_RELATIVE_URLS'] = True
 
-#controllers
+
+# controllers
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/interview.html")
-def interview():
-    return render_template('interview.html')
 
 @app.route("/thanks.html")
 def thanks():
     return render_template('thanks.html')
 
+
 @app.route("/confirm.html")
 def confirm():
     return render_template('confirm.html')
+
 
 #launch
 if __name__ == "__main__":
